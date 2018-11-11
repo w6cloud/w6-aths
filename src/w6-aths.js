b41783df53980bc5978577a526dbff4e493810dd
Treize Interactif <pierre@13i.fr>
2018-11-11 20:01:46 +0100

    Comments

--- src/w6-aths.js
+++ src/w6-aths.js
@@ -3,52 +3,55 @@
   
   var navigator = globals.navigator;
   
-  var aths = {};
-  
-  aths.supported = null;
-  
-  aths.installed = null;
-  
-  aths.message = null;
-  
-  aths.platform = null;
-  
-  aths.position = ['bottom', 'center'];
-  
-  aths.lang = 'en';
+  var aths = {
+    supported: null,                 // True if the platform is supported
+    installed: null,                 // True if the app is already installed
+    message:   null,                 // The platform specific install instructions
+    platform:  null,                 // The platform name (ios / android)
+    position:  ['bottom', 'center'], // Th message position
+    lang:      'en'                  // The content language
+  };
   
   aths.messages = {
     en: {
-      ios: '<strong>Tap on the share icon</strong>, then tap on <q>Add to Home Screen</q>.',
-      android: '<strong>Tap on &#8942;</strong> and then tap on <q>Add to Home screen</q>.',
-      installed: 'Already installed',
+      ios:           '<strong>Tap on the share icon</strong>, then tap on <q>Add to Home Screen</q>.',
+      android:       '<strong>Tap on &#8942;</strong> and then tap on <q>Add to Home screen</q>.',
+      installed:     'Already installed',
       not_supported: 'Add to home screen instructions are not available for your device.'
     },
     fr: {
-      ios: '<strong>Appuyez sur l\'icône partage</strong>, puis appuyez sur <q>Sur l\'écran d\'accueil</q>.',
-      android: '<strong>Appuyez sur &#8942;</strong>, puis appuyez sur <q>Ajouter à l\'écran d\'accueil</q>.',
-      installed: 'Application déjà installée',
+      ios:           '<strong>Appuyez sur l\'icône partage</strong>, puis appuyez sur <q>Sur l\'écran d\'accueil</q>.',
+      android:       '<strong>Appuyez sur &#8942;</strong>, puis appuyez sur <q>Ajouter à l\'écran d\'accueil</q>.',
+      installed:     'Application déjà installée',
       not_supported: 'Les instructions pour l\'ajout à l\'écran d\'accueil ne sont pas disponibles pour votre plateforme.'
     }
   };
   
   aths.positions = {
-    ios: ['bottom', 'center'],
+    ios:     ['bottom', 'center'],
     android: ['top', 'right']
   };
   
+  /**
+   * Get translated strings
+   */
   aths._ = function(key){
     return this.messages[this.lang][key];
   };
   
-  aths.init = function(lang){
-    if(!lang){
-      lang = 'en';
+  /**
+   * Set the content language
+   */
+  aths.setLanguage = function(lang){
+    if(lang in this.messages){
+      this.lang = lang;
     }
-    this.lang = lang;
     return this;
   };
   
+  /**
+   * Determine if platform is supported (OS + Browser)
+   */
   aths.isSupported = function(){
     if(this.supported !== null){
       return this.supported;
@@ -81,6 +84,9 @@
     return this.supported;
   };
   
+  /**
+   * Determine if the app is already added to hoeme screen
+   */
   aths.isInstalled = function(){
     if(this.installed !== null){
       return this.installed;
@@ -96,6 +102,9 @@
     return this.installed;
   };
   
+  /**
+   * Get the appropriate install instructions depending on the platform
+   */
   aths.getMessage = function(){
     if(this.message !== null){
       return this.message;
@@ -109,6 +118,9 @@
     return this.message;
   };
   
+  /**
+   * Get the position where the message muste be displayed (near the buttons)
+   */
   aths.getPosition = function(){
     if(this.position !== null){
       return this.position;
@@ -118,6 +130,11 @@
     return this.position;
   };
   
+  /**
+   * Get the platform :
+   * - ios : iOS + Safari
+   * - android : Android + Chrome
+   */
   aths.getPlatform = function(){
     if(this.platform !== null){
       return this.platform;
